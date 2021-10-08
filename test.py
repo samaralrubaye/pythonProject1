c1962515b3f689305d747d4c8e44f47c3413db6f
samar <engsamaralrubaye@gmail.com>
2021-10-08 22:21:23 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import folium
+import geocoder
+import pygeoip
+g= geocoder.ip("37.236.252.248")
+
+myaddress=g.latlng
+print(myaddress)
+
+
+m=folium.Map(location=myaddress, zoom_start=4)
+
+folium.Marker(myaddress).add_to(m)
+m.save("m.html")
