f63ca44570a09f8840475bed8c048c984fbb9600
samar <engsamaralrubaye@gmail.com>
2022-04-16 12:57:03 +0100

    can read the id for summary

--- view/mytable.py
+++ view/mytable.py
@@ -1,8 +1,11 @@
+import sys
 from typing import overload
 from PyQt5.QtWidgets import *
 
 
 from MyQwidgetItem import MyQwidgetItem
+from criminalSummary import summary
+from filteremail import checking
 
 class MyTableWidget(QTableWidget):
     def __init__(self):
@@ -17,8 +20,19 @@ class MyTableWidget(QTableWidget):
     def handleItemClick(self, item):
         try:
              print(str(item.ID))
+            #  pop=summary(self,item.ID)
+            #  pop.show()
+            #  pop=checking()
+            #  pop.show()
+             
+
         except:
             print('tt')
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyTableWidget()
+    sys.exit(app.exec_())  
+    #sys.exit(app.exec_())
         
        
     #     self.cellClicked.connect(lambda:self.cell_was_clicked(self.currentRow(),self.currentColumn()))
