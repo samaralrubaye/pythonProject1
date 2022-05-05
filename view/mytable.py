0a9d8197312f3bb1ef693ed819186285d927c6ae
samar <engsamaralrubaye@gmail.com>
2022-05-05 19:55:11 +0100

    links working

--- view/mytable.py
+++ view/mytable.py
@@ -18,11 +18,8 @@ import pandas as pd
 
 from viber import viber
 
-from viberSummary import vibersummary
-from allEmail import allEmail
-from allWhatsApp import allWhatsApp
 
-from allViber import allViber
+from mytest import SplashScreen
 
 
 
@@ -57,178 +54,20 @@ class MyTableWidget(QTableWidget):
     def Type(self, value):
         self.type = value
 
-    def initUI(self):
-        self.title = 'PyQt5 table'
-        self.left = 20000
-        self.top = 200
-        self.width = 1000
-        self.height = 400
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        
-        self.layout = QVBoxLayout()
-        self.tableWidget = QTableWidget()
-      
-        
-        self.buttonCVS = QPushButton()
-        self.btnclose = QPushButton()
-        self.buttonCVS.setText("Import as a CVS")
-        self.btnclose.setText("close") 
-        self.buttonCVS.move(64,32)
-        self.layout.addWidget(self.buttonCVS) 
-        self.layout.addWidget(self.tableWidget)
-        self.layout.addWidget( self.btnclose)  
-        self.setLayout(self.layout) 
-        self.show()
-        self.btnclose.clicked.connect(self.hidding)
-        
-
     def handleItemClick(self, item):
         try:
-            # self.progressBar.setValue(self.counter)
-            # if self.counter == int(self.n * 0.3):
-            #     self.labelDescription.setText('<strong>Working on Task #2</strong>')
-            # elif self.counter == int(self.n * 0.6):
-            #     self.labelDescription.setText('<strong>Working on Task #3</strong>')
-            # elif self.counter >= self.n:
-            #  self.timer.stop()
-            #  self.close()
+            
+             self.SplashScreen= SplashScreen(self.CaseID,self.Type,item.ID)
+             self.SplashScreen.show()
 
-            #  time.sleep(1)
-
-            # self.SplashScreen= SplashScreen()
-            # self.SplashScreen.show()
-
-            #  self.counter += 1
-         
-             
-             self.title = 'PyQt5 table'
-             self.left = 20000
-             self.top = 200
-             self.width = 1000
-             self.height = 400
-             self.initUI()
-             self.tableWidget.setRowCount(10)
-             self.tableWidget.setColumnCount(10)
-             self.tableWidget.setHorizontalHeaderItem(0, QTableWidgetItem("Sender first Name"))
-             self.tableWidget.setHorizontalHeaderItem(1, QTableWidgetItem("Sender last Name"))
-             self.tableWidget.setHorizontalHeaderItem(2, QTableWidgetItem(" message"))
-  
-             self.tableWidget.setColumnWidth(0,200)
-             self.tableWidget.setColumnWidth(1,200)
-             self.tableWidget.setColumnWidth(2,200)
-             
-             if (self.Type == 1):
-              self.GetEmails(item.ID)
-        
-             if (self.Type == 2):
-              self.WhatsApploading(item.ID)
-              
-             if (self.Type == 3):
-              self.vibercall(item.ID)
-            #  pop=summary(self,item.ID)
-            #  pop.show()
-            #  pop=checking()
-            #  pop.show()
         except:
             print('tt')
 
-    def GetEmails(self,senderID):
-        #emails=allEmail.getAllEmails(self)
-        
-        emails=allEmail.getEmailsBySenderID(self,self.CaseID,senderID)
-
-        row=0
-        self.tableWidget.setRowCount(len(emails))
-      
-        for i in emails:
-               
-              # index = PyQt5.QtCore.QPersistentModelIndex(
-               # self.tableWidget.model().index(row, 1))
-            
-            # row.setitem(row,1, QTableWidgetItem(i.FromEmail_lastname))
-
-            # self.tableWidget.rowAt[i] = row
-
-               fname =  MyQwidgetItem(i.FromEmail_firstName)
-               fname.ID =i.FromEmail_ID
-               self.tableWidget.setItem(row,0,fname)
-
-               self.tableWidget.setItem(row,1, MyQwidgetItem(i.FromEmail_lastname))
-               self.tableWidget.setItem(row,2, MyQwidgetItem(i.FromEmail_Email))
-               self.tableWidget.setItem(row,3, MyQwidgetItem(i.FromEmail_content_text))
-               self.tableWidget.setItem(row,4, MyQwidgetItem(i.FromEmail_timeDate))
-               self.tableWidget.setItem(row,5, MyQwidgetItem(i.ToEmail_firstName))
-               self.tableWidget.setItem(row,6, MyQwidgetItem(i.ToEmail_lastname))
-               self.tableWidget.setItem(row,7, MyQwidgetItem(i.ToEmail_Email))
-               self.tableWidget.setItem(row,8, MyQwidgetItem(i.ToEmail_timeDate))
-               self.tableWidget.setItem(row,9, MyQwidgetItem(i.ToEmail_timeDate))
-               self.tableWidget.setItem(row,10, MyQwidgetItem(i.BccEmail_Email))
-               self.tableWidget.setItem(row,11, MyQwidgetItem(i.CcEmail_Email))
-               row=row+1
-            
-               self.tableWidget.move(0,0)
-               
-    def vibercall(self, senderID):
-        vibers=allViber.getVibersBySenderID(self,self.CaseID,senderID)
-        row=0
-        self.tableWidget.setRowCount(len(vibers))
-        for j in vibers:
-            fname =  MyQwidgetItem(j.FromViber_Msg_FirstName)
-            fname.ID =j.FromViber_Msg_ID
-            self.tableWidget.setItem(row,0,fname)
-          
-            self.tableWidget.setItem(row,1, MyQwidgetItem(j.FromViber_Msg_LastName))
-            self.tableWidget.setItem(row,2, MyQwidgetItem(j.FromViber_Msg_number))
-            self.tableWidget.setItem(row,3, MyQwidgetItem(j.ToViber_Msg))
-            self.tableWidget.setItem(row,4, MyQwidgetItem(j.ToViber_Msg_DateandTime))
-            self.tableWidget.setItem(row,5, MyQwidgetItem(j.ToViber_Msg_FirstName))
-            self.tableWidget.setItem(row,6, MyQwidgetItem(j.ToViber_Msg_LastName))
-            self.tableWidget.setItem(row,7, MyQwidgetItem(j.ToViber_Msg_number))
-            self.tableWidget.setItem(row,8, MyQwidgetItem(j.ToViber_Msg))
-            row=row+1
-            self.tableWidget.move(0,0)
-     
-     
-    def WhatsApploading(self, senderID):
-          WhatsApps= allWhatsApp.getWhatsAppBySenderID(self,self.CaseID,senderID)
-          row=0
-          self.tableWidget.setRowCount(len(WhatsApps))
-          for j in WhatsApps:
-            fname =  MyQwidgetItem(j.FromWhatsApp_Msg_FirstName)
-            fname.ID =j.FromWhatsApp_Msg_ID
-            self.tableWidget.setItem(row,0,fname)
-            self.tableWidget.setItem(row,1, MyQwidgetItem(j.FromWhatsApp_Msg_LastName))
-            self.tableWidget.setItem(row,2, MyQwidgetItem(j.FromWhatsApp_Msg_number))
-            self.tableWidget.setItem(row,3, MyQwidgetItem(j.FromWhatsApp_Msg))
-            self.tableWidget.setItem(row,4, MyQwidgetItem(j.FromWhatsApp_Msg_DateandTime))
-            self.tableWidget.setItem(row,5, MyQwidgetItem(j.ToWhatsApp_Msg_FirstName))
-            self.tableWidget.setItem(row,6, MyQwidgetItem(j.ToWhatsApp_Msg_LastName))
-            self.tableWidget.setItem(row,7, MyQwidgetItem(j.ToWhatsApp_Msg_number))
-            self.tableWidget.setItem(row,8, MyQwidgetItem(j.ToWhatsApp_Msg_DateandTime))
-            row=row+1   
-            self.tableWidget.move(0,0)
 
 
-    def hidding(self):
-        # bookmark = QtWidgets.QDialog()
-        self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyTableWidget()
     sys.exit(app.exec_())  
-    #sys.exit(app.exec_())
-        
-       
-    #     self.cellClicked.connect(lambda:self.cell_was_clicked(self.currentRow(),self.currentColumn()))
-    #     print(type(self.currentRow()))
-    #     print(type(self.currentColumn()))
-  
-
-    # def cell_was_clicked(self, row, column):
-    #         item = self.itemAt(self,row, column)
-    #         print(type(self.currentRow()))
-    #         print(type(self.currentColumn()))
-    #         self.ID = item.text()
         
--- view/mytest.py
+++ view/mytest.py
@@ -28,11 +28,17 @@ import time
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar, QLabel, QFrame, QHBoxLayout, QVBoxLayout,QTableWidget
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import *
+from MyQwidgetItem import *
 
-
+from allViber import allViber
+from allEmail import allEmail
+from allWhatsApp import allWhatsApp
 class SplashScreen(QWidget):
-    def __init__(self):
+    def __init__(self,cID = None,ctype = None,iID = None):
         super().__init__()
+        self.caseid = cID        
+        self.type = ctype
+        self.itemid = iID
         self.setWindowTitle('Spash Screen Example')
         self.setFixedSize(1100, 500)
         self.setWindowFlag(Qt.FramelessWindowHint)
@@ -45,7 +51,33 @@ class SplashScreen(QWidget):
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.loading)
-        self.timer.start(30)
+        self.timer.start(5)
+
+    @property
+    def ItemID(self):
+        return self.itemid
+
+    @ItemID.setter
+    def ItemID(self, value):
+        self.itemid = value
+ 
+
+    @property
+    def CaseID(self):
+        return self.caseid
+
+    @CaseID.setter
+    def CaseID(self, value):
+        self.caseid = value
+ 
+ 
+    @property
+    def Type(self):
+        return self.type
+
+    @Type.setter
+    def Type(self, value):
+        self.type = value
 
     def initUI(self):
         layout = QVBoxLayout()
@@ -86,6 +118,9 @@ class SplashScreen(QWidget):
         self.labelLoading.setAlignment(Qt.AlignCenter)
         self.labelLoading.setText('loading...')
 
+
+
+
     def loading(self):
         self.progressBar.setValue(self.counter)
 
@@ -99,19 +134,22 @@ class SplashScreen(QWidget):
 
             time.sleep(1)
 
-            self.myApp = MyApp()
+            self.myApp = MyApp(self.caseid,self.Type,self.ItemID)
             self.myApp.show()
 
         self.counter += 1
 class MyApp(QTableWidget):
 
-    def __init__(self):
+    def __init__(self,cID = None,ctype = None,iID = None):
         super().__init__()
+        self.caseid = cID        
+        self.type = ctype
+        self.itemid = iID
         self.title = 'PyQt5 - QTableWidget'
         self.left = 0
         self.top = 0
-        self.width = 300
-        self.height = 200
+        self.width = 800
+        self.height = 600
    
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -124,25 +162,137 @@ class MyApp(QTableWidget):
    
         #Show window
         self.show()
-   
+
+    @property
+    def ItemID(self):
+        return self.itemid
+
+    @ItemID.setter
+    def ItemID(self, value):
+        self.itemid = value
+ 
+
+    @property
+    def CaseID(self):
+        return self.caseid
+
+    @CaseID.setter
+    def CaseID(self, value):
+        self.caseid = value
+ 
+ 
+    @property
+    def Type(self):
+        return self.type
+
+    @Type.setter
+    def Type(self, value):
+        self.type = value
+
     #Create table
     def createTable(self):
+        self.title = 'PyQt5 table'
+        self.left = 20000
+        self.top = 200
+        self.width = 1000
+        self.height = 400
         self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(10)
+        self.tableWidget.setColumnCount(10)
+        self.tableWidget.setHorizontalHeaderItem(0, MyQwidgetItem("Sender first Name"))
+        self.tableWidget.setHorizontalHeaderItem(1, MyQwidgetItem("Sender last Name"))
+        self.tableWidget.setHorizontalHeaderItem(2, MyQwidgetItem(" message"))
   
-        #Row count
-        self.tableWidget.setRowCount(4) 
-  
-        #Column count
-        self.tableWidget.setColumnCount(2)  
-  
+        self.tableWidget.setColumnWidth(0,200)
+        self.tableWidget.setColumnWidth(1,200)
+        self.tableWidget.setColumnWidth(2,200)
         
-   
-        #Table will fit the screen horizontally
-        self.tableWidget.horizontalHeader().setStretchLastSection(True)
-        self.tableWidget.horizontalHeader().setSectionResizeMode(
-            QHeaderView.Stretch)        
+        if (self.Type == 1):
+         self.GetEmails(self.ItemID)
         
-             
+        if (self.Type == 2):
+         self.WhatsApploading(self.ItemID)
+         
+        if (self.Type == 3):
+         self.vibercall(self.ItemID)
+      
+
+
+    def GetEmails(self,senderID):
+        #emails=allEmail.getAllEmails(self)
+        
+        emails=allEmail.getEmailsBySenderID(self,self.CaseID,senderID)
+
+        row=0
+        self.tableWidget.setRowCount(len(emails))
+      
+        for i in emails:
+               
+              # index = PyQt5.QtCore.QPersistentModelIndex(
+               # self.tableWidget.model().index(row, 1))
+            
+            # row.setitem(row,1, QTableWidgetItem(i.FromEmail_lastname))
+
+            # self.tableWidget.rowAt[i] = row
+
+               fname =  MyQwidgetItem(i.FromEmail_firstName)
+               fname.ID =i.FromEmail_ID
+               self.tableWidget.setItem(row,0,fname)
+
+               self.tableWidget.setItem(row,1, MyQwidgetItem(i.FromEmail_lastname))
+               self.tableWidget.setItem(row,2, MyQwidgetItem(i.FromEmail_Email))
+               self.tableWidget.setItem(row,3, MyQwidgetItem(i.FromEmail_content_text))
+               self.tableWidget.setItem(row,4, MyQwidgetItem(i.FromEmail_timeDate))
+               self.tableWidget.setItem(row,5, MyQwidgetItem(i.ToEmail_firstName))
+               self.tableWidget.setItem(row,6, MyQwidgetItem(i.ToEmail_lastname))
+               self.tableWidget.setItem(row,7, MyQwidgetItem(i.ToEmail_Email))
+               self.tableWidget.setItem(row,8, MyQwidgetItem(i.ToEmail_timeDate))
+               self.tableWidget.setItem(row,9, MyQwidgetItem(i.ToEmail_timeDate))
+               self.tableWidget.setItem(row,10, MyQwidgetItem(i.BccEmail_Email))
+               self.tableWidget.setItem(row,11, MyQwidgetItem(i.CcEmail_Email))
+               row=row+1
+            
+               self.tableWidget.move(0,0)
+
+    def vibercall(self, senderID):
+        vibers=allViber.getVibersBySenderID(self,self.CaseID,senderID)
+        row=0
+        self.tableWidget.setRowCount(len(vibers))
+        for j in vibers:
+            fname =  MyQwidgetItem(j.FromViber_Msg_FirstName)
+            fname.ID =j.FromViber_Msg_ID
+            self.tableWidget.setItem(row,0,fname)
+          
+            self.tableWidget.setItem(row,1, MyQwidgetItem(j.FromViber_Msg_LastName))
+            self.tableWidget.setItem(row,2, MyQwidgetItem(j.FromViber_Msg_number))
+            self.tableWidget.setItem(row,3, MyQwidgetItem(j.ToViber_Msg))
+            self.tableWidget.setItem(row,4, MyQwidgetItem(j.ToViber_Msg_DateandTime))
+            self.tableWidget.setItem(row,5, MyQwidgetItem(j.ToViber_Msg_FirstName))
+            self.tableWidget.setItem(row,6, MyQwidgetItem(j.ToViber_Msg_LastName))
+            self.tableWidget.setItem(row,7, MyQwidgetItem(j.ToViber_Msg_number))
+            self.tableWidget.setItem(row,8, MyQwidgetItem(j.ToViber_Msg))
+            row=row+1
+            self.tableWidget.move(0,0)
+     
+     
+    def WhatsApploading(self, senderID):
+          WhatsApps= allWhatsApp.getWhatsAppBySenderID(self,self.CaseID,senderID)
+          row=0
+          self.tableWidget.setRowCount(len(WhatsApps))
+          for j in WhatsApps:
+            fname =  MyQwidgetItem(j.FromWhatsApp_Msg_FirstName)
+            fname.ID =j.FromWhatsApp_Msg_ID
+            self.tableWidget.setItem(row,0,fname)
+            self.tableWidget.setItem(row,1, MyQwidgetItem(j.FromWhatsApp_Msg_LastName))
+            self.tableWidget.setItem(row,2, MyQwidgetItem(j.FromWhatsApp_Msg_number))
+            self.tableWidget.setItem(row,3, MyQwidgetItem(j.FromWhatsApp_Msg))
+            self.tableWidget.setItem(row,4, MyQwidgetItem(j.FromWhatsApp_Msg_DateandTime))
+            self.tableWidget.setItem(row,5, MyQwidgetItem(j.ToWhatsApp_Msg_FirstName))
+            self.tableWidget.setItem(row,6, MyQwidgetItem(j.ToWhatsApp_Msg_LastName))
+            self.tableWidget.setItem(row,7, MyQwidgetItem(j.ToWhatsApp_Msg_number))
+            self.tableWidget.setItem(row,8, MyQwidgetItem(j.ToWhatsApp_Msg_DateandTime))
+            row=row+1   
+            self.tableWidget.move(0,0)    
 
 
 if __name__ == '__main__':
